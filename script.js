f8c83501d591946119da96377b6b45c15438952d
TheProof68 <113829552+TheProof68@users.noreply.github.com>
2024-08-14 21:29:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Ajoute des fonctionnalités JavaScript si nécessaire
+    // Par exemple, ajouter un effet de son lorsque l'utilisateur survole les sections
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach(button => {
+        button.addEventListener('mouseover', () => {
+            // Jouer un son effrayant ici si nécessaire
+        });
+    });
+});
